611aed9623e67f5a395660eebae96c2bf978fb18
Werner Gehrke Junior <werner.junior@outlook.com>
2021-03-17 20:52:39 -0300

    Layout Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header () {
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+
+export function Header (props:  HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money"/>
-        <button type="button">
+        <button type="button" onClick={props.onOpenNewTransactionModal} >
           Nova transação
         </button>
       </Content>
