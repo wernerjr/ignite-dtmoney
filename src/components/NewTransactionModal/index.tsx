06f05feac88592d17e5faf38174c94e0106cf636
Werner Gehrke Junior <werner.junior@outlook.com>
2021-03-17 21:25:40 -0300

    Adicionar e listar transações

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -12,7 +13,24 @@ interface NewTransactionModalProps {
 
 
 export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+  
   const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {        
+      title, 
+      value, 
+      type, 
+      category      
+    };
+
+    api.post('transactions', data);
+  }
   
   return (
     <Modal 
@@ -24,11 +42,20 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
       <button type="button" onClick={onRequestClose} className="react-modal-close">
         <img src={closeImg} alt="Fechar modal"/>
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
       <h2>Cadastrar Transação</h2>
 
-        <input placeholder="Título"/>
-        <input type="number" placeholder="Valor"/>
+        <input 
+          placeholder="Título"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+        <input 
+          type="number" 
+          placeholder="Valor"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -52,7 +79,11 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
         </TransactionTypeContainer>
 
 
-        <input placeholder="Categoria"/>
+        <input 
+          placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
 
 
         <button type="submit">Cadastrar</button>
--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,13 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
-export function TransactionTable() {
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
 
-useEffect(() => {
-  api.get('transactions')
-  .then(response => console.log(response.data));
-}, [])
+
+export function TransactionTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => setTransactions(response.data.transactions));
+  }, [])
 
   return (
     <Container>
@@ -21,24 +32,33 @@ useEffect(() => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimentod e WebSite</td>
-            <td>R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimentod e WebSite</td>
-            <td className='deposit' >R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimentod e WebSite</td>
-            <td className='withdraw' >- R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
+
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type} >
+                {
+                  new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(transaction.amount)
+                }
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+              {
+                  new Intl.DateTimeFormat('pt-BR')
+                  .format(new Date(transaction.createdAt))
+                }
+              </td>
+            </tr>
+          ))}
+
+
+
+
+
+
         </tbody>
       </table>
     </Container>
