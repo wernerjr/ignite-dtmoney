cde99b49da2f4f11abfa3c1ece2476b01cc12630
Werner Gehrke Junior <werner.junior@outlook.com>
2021-03-18 22:21:39 -0300

    Adicionar diretório responsável por mock

--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer, Model } from 'miragejs';
 import { App } from './App';
+import { mirageMock } from './mock';
 
-createServer({
-  models: {
-    transaction: Model
-  },
-
-  seeds(server) {
-    server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Freelance de website',
-          type: 'deposit',
-          category: 'Dev',
-          amount: 6000, 
-          createdAt: new Date('2021-01-01 09:00:00')
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1100, 
-          createdAt: new Date('2021-01-15 11:00:00')
-        }
-      ]
-    })
-  },
-
-  routes() {
-    this.namespace = 'api';
-
-    this.get('/transactions', () => {
-      return this.schema.all('transaction');
-    })
-
-    this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody);
-      data.createdAt = new Date();
-      return schema.create('transaction', data);
-    })
-  }
-});
+const isDevelopment = process.env.NODE_ENV !== 'production';
+if(isDevelopment){
+  mirageMock();
+}
 
 ReactDOM.render(
   <React.StrictMode>
